57fbc8ee59ad3da428962940ca9d0cbbe65cdbb9
RunWang0402 <mwwpositive1000@gmail.com>
2024-10-28 14:05:47 -0700

    new tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("adding expense", ()=>{
+  render(<App/>);
+  const createExpenseButton = screen.getByText("Save");
+  const createExpense = screen.getByLabelText("Name");
+  const createCost = screen.getByLabelText("Cost");
+  fireEvent.change(createExpense, {target:{value:"apple"}});
+  fireEvent.change(createCost,{target:{value:300}});
+  fireEvent.click(createExpenseButton);
+
+  const newExpenseName = screen.getByText("apple");
+  const newExpenseCost = screen.getByText("$300");
+  expect(newExpenseName).toBeInTheDocument();
+
+  expect(newExpenseCost).toBeInTheDocument();
+ 
+})
+
+test("total spent and remaining updates", ()=>{
+render (<App/>);
+
+const createExpenseButton = screen.getByText("Save");
+  const createExpense = screen.getByLabelText("Name");
+  const createCost = screen.getByLabelText("Cost");
+  fireEvent.change(createExpense, {target:{value:"apple"}});
+  fireEvent.change(createCost,{target:{value:300}});
+  fireEvent.click(createExpenseButton);
+
+  const newExpenseName = screen.getByText("apple");
+  const newExpenseCost = screen.getByText("$300");
+  const totalBudgetText = screen.getByText(/Budget: \$/i);
+  const remainingText = screen.getByText(/Remaining: \$/i);
+  const spentSoFarText = screen.getByText(/Spent so far: \$/i);
+
+  // const totalBudget = parseInt(totalBudgetText.textContent?.replace(/[^0-9]/g, "") || "0", 10);
+  const remainingNumber = parseInt(remainingText.textContent?.replace(/[^0-9]/g, "") || "0", 10);
+  const spentSoFarNumber = parseInt(spentSoFarText.textContent?.replace(/[^0-9]/g, "") || "0", 10);
+  const totalBudget = parseInt(totalBudgetText.textContent?.replace(/[^0-9]/g, "") || "0", 10);
+ 
+  expect (300).toBe(spentSoFarNumber);
+  expect(totalBudget-300).toBe(remainingNumber);
+
+
+})
 
 test("detele expense", () => {
   render(<App />);
@@ -43,8 +92,6 @@ test("delete expense update", () => {
 test("budget balance verification", () => {
   render(<App />);
 
-  
-
   const purchases = {
     "apple": "50",
     "pear" : "40",
@@ -76,5 +123,4 @@ test("budget balance verification", () => {
 
 
 });
-
 
